8af63502d20c182873b0d6320fb57d2a4a6c7ad2
web3 vicky <vicky.web3@gmail.com>
2022-08-06 22:40:28 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { Component, useReducer } = React;
+const { render } = ReactDOM;
+
+function App() {
+    return(<div>
+
+    </div>)
+}
+
+ReactDom.render(<App />, document.getElementById("root"));
